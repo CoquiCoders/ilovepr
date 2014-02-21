d054c82707e9ee6e6f011600476b263fd7ec51cb
Ainsley <ainsley.wagoner@gmail.com>
2014-02-21 18:42:27 -0400

    Adding isotope jquery library. Also adding favicon

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 
-  // Place JavaScript code here...
-
+	var container = $('.notes');
+	
+	console.log(container.width());
+	
+	container.isotope({
+		itemSelector: '.note',
+		// options...
+		resizable: false, // disable normal resizing
+		// set columnWidth to a percentage of container width
+		masonry: { columnWidth: container.width() / 50 }
+	});
+	
+	// update columnWidth on window resize
+	$(window).smartresize(function(){
+	  container.isotope({
+	    // update columnWidth to a percentage of container width
+	    masonry: { columnWidth: container.width() / 50 }
+	  });
+	});
 });
