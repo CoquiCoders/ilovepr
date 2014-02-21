2f8669bf7656e13d9bfdcabb173c881f64122c94
Maksim Pecherskiy <max@maksimize.com>
2014-02-21 09:52:03 -0400

    Adding socket stuff, still need to add the js.

--- app.js
+++ app.js
@@ -33,7 +33,11 @@ var passportConf = require('./config/passport');
  * Create Express server.
  */
 
+//var app = express();
 var app = express();
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
 /**
  * Mongoose configuration.
@@ -169,6 +173,21 @@ app.get('/auth/venmo/callback', passport.authorize('venmo', { failureRedirect: '
  * Start Express server.
  */
 
-app.listen(app.get('port'), function() {
+//app.listen(app.get('port'), function() {
+server.listen(app.get('port'), function() {
   console.log("✔ Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
+});
+
+io.configure(function() {
+  io.set('transports', ['websocket']);
+});
+
+io.sockets.on('connection', function(socket) {
+  socket.emit('greet', { hello: 'Hey, Mr.Client!' });
+  socket.on('respond', function(data) {
+    console.log(data);
+  });
+  socket.on('disconnect', function() {
+    console.log('Socket disconnected');
+  });
 });
