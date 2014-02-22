fd3ec51af0facd789784e72000490d016ada041d
Maksim Pecherskiy <max@maksimize.com>
2014-02-22 13:38:55 -0400

    Testing adding a new controller callback as per stackoverflow.

--- controllers/note.js
+++ controllers/note.js
@@ -47,4 +47,12 @@ exports.postNewNoteForm = function(req, res, next) {
 exports.getNotes = function(req, res, next) {
 };
 
-
+exports.respond = function (socket_io) {
+  // now we can do whatever we want:
+  console.log('responding');
+  socket_io.on('news',function(newsreel){
+  // as is proper, protocol logic like
+  // this belongs in a controller:
+    socket.broadcast.emit(newsreel);
+  });
+};
