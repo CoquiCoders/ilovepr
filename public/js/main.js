5af45b187941d6743080812bdf9ef4b505b79b4c
Maksim Pecherskiy <max@maksimize.com>
2014-03-08 10:51:26 -0800

    Adding intents for tweeting and tweet button.

--- public/js/main.js
+++ public/js/main.js
@@ -14,6 +14,16 @@ var prLover = {
     if (noteData.twitterHandle) {
       newNote = newNote + "<div class='note-twitter-handle'><a href='http://twitter.com/" + noteData.twitterHandle + "' target='_blank'>@" + noteData.twitterHandle + "</a></div>";
     }
+    var tweetLink = {
+      url: 'http://bit.ly/ilovepr',
+      text: 'Yo Me Quedo en Puerto Rico porque ' + noteData.text,
+      hashtags: 'ilovepr'
+    }
+    var tweetUrl = 'https://twitter.com/intent/tweet?' + $.param(tweetLink, true);
+    newNote = newNote + '<div class="tweet-link pull-right"><a href="' + tweetUrl + '"><img src="img/bird_gray_32.png"/></a></div>';
+    //<p><a href="https://twitter.com/intent/tweet?in_reply_to=51113028241989632">Reply</a></p>
+// http://bit.ly/ilovepr
+// http%3A%2F%2Fbit.ly/ilovepr%2F
     newNote = newNote + "</li>";
 
     return newNote;
@@ -120,6 +130,9 @@ var prLover = {
       .isotope({ sortBy: 'original-order' });
   },
 
+  openTweetWindowWithData: function(tweetData) {
+
+  },
   init: function() {
     // Setup.
     // Grab Loaded Notes
@@ -153,6 +166,10 @@ var prLover = {
       event.stopPropration;
       self.createNewNote().then(function(response) {
         if (!response.errors) {
+          // If tweet is checked, tweet.
+          if ($('#tweetThisNote').is(':checked')) {
+            prLover.openTweetWindowWithData({});
+          }
           // Clear out the fields.
           $('#new-note-form #noteText').val('');
           $('#new-note-form #twitterHandle').val('');
