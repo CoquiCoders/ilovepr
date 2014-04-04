d0fcc7dbeafcacf252a538788a9cc63f8489eb44
Maksim Pecherskiy <max@maksimize.com>
2014-04-04 16:04:44 -0700

    Stupid spacing.

--- controllers/note.js
+++ controllers/note.js
@@ -61,12 +61,12 @@ exports.updateNote = function(req, res) {
   var voteOp = req.body.voteCount;
   var update = {};
   if (voteOp == '+1'){
-	update = { $inc: { votes: 1 } } ;
+  	update = { $inc: { votes: 1 } } ;
   }
   Note.update(query, update, {}, function (err, numberAffected, raw) {
-	console.log(err);
-	console.log(numberAffected);
-	console.log(raw);
+  	console.log(err);
+  	console.log(numberAffected);
+  	console.log(raw);
   })
 
 };
